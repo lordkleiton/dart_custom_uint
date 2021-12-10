deadf30ffe8d3496e07f17f3f50bddf9186bfcfa
lord <m_s_r@outlook.com>
2021-12-09 21:35:12 -0300

    iniciando repo

---
+++ lib/dart_uint.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_uint;
+
+export 'src/dart_uint_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
